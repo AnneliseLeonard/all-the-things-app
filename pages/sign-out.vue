47b844293adab946baa33ccb6cf1cf6039786df9
AnneliseLeonard <annelise.leonard2@gmail.com>
2025-07-19 13:53:37 -0500

    sign out

---
+++ pages/sign-out.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+
+onMounted(() => {
+	authStore.signOut();
+});
+</script>
+
+<template>
+	<div class="card bg-base-200 container mt-4 min-h-72 text-center justify-center items-center mx-auto flex flex-col gap-4">
+		<span class="loading loading-spinner loading-md" />
+	</div>
+</template>
